9ae8c76b86243be5807c4407d179f456c91ea53d
Jonathan9168 <77795437+Jonathan9168@users.noreply.github.com>
2023-08-23 22:26:54 +0100

    Add files via upload

---
+++ csgo.py
@@ -0,0 +1,224 @@
+import time
+import requests
+import pandas as pd
+from datetime import datetime
+from urllib.parse import quote
+from openpyxl import load_workbook
+
+
+def get_current_item_value(name, max_retries=3, ttw=2.5):
+    """
+
+    Retrieves CSGO item price from Steam endpoint
+
+    name: item name
+    max_retries: maximum number of times to attempt to fetch price data for item if the request was unsuccessful
+    ttw (time to wait): to prevent error 429, add wait time between requests
+    response: reply from Steam endpoint
+
+    response format (successful):
+     {
+        "success": true,
+        "lowest_price": "£4.22",
+        "volume": "250",
+        "median_price": "£4.37"
+    }
+
+    response format (unsuccessful):
+    {
+        "success": false
+    }
+
+    total script time ~ ttw * no. unique items (ttw= 0 can be used for < 18 unique items consistently before error 429)
+    :returns: current Steam market listing price (successful) or False (unsuccessful)
+
+    """
+
+    response_data = None
+
+    for _ in range(max_retries):
+        try:
+            link = f"https://steamcommunity.com/market/priceoverview/?currency=2&appid=730&market_hash_name={quote(name)}"
+            response = requests.get(link)
+            time.sleep(ttw)
+
+            if response.ok:
+
+                response_data = response.json()
+
+                if response_data["success"] is True:
+                    return float(response_data['lowest_price'][1:])
+                else:
+                    continue
+
+        except KeyError as e:
+            if "median_price" in response_data:
+                return float(response_data['median_price'][1:])
+
+            print(response_data)
+            print("An error occurred:", e)
+
+    return False
+
+
+def percentage_change(old_value, new_value):
+    """
+
+    Calculates percentage change between two values
+
+    old_value: previous value of some variable
+    new_value: updated value of some variable
+
+    :returns: percentage difference between old and new value (not multiplied by 100 as Excel percentage format does this)
+
+    """
+
+    # use 0.01 as a placeholder to avoid division by 0 error
+    if old_value == 0:
+        old_value = 0.01
+
+    return (new_value - old_value) / old_value
+
+
+def calculate_expected_profit():
+    """
+    Calculates the expected profit in cell L1 given by the formula: =SUMIFS(H:H, I:I, "N/A") * 0.85
+
+    We take the sum of values in column H (Current - Purchase [DIFF])(difference in current market value and purchase price)
+    where the corresponding value in column I (Sold Price) is "N/A",
+    this means that only items that have not been sold are factored into the calculation.
+
+    Finally, the sum is multiplied by 0.85 to account for Steam selling fees (5% Steam + 10% game fee [CSGO])
+
+    :returns: The expected profit if the items are sold at Steam market value
+
+    """
+    return 0.85 * sum([float(current_value_steam.value) - float(purchase_price.value) for
+                       purchase_price, current_value_steam, sold_price in zip(ws['E'][1:], ws['F'][1:], ws['I'][1:]) if
+                       sold_price.value == "N/A"])
+
+
+def update_dataframe():
+    """
+
+    Updates dataframe clone of Excel file with new item values
+
+    item: item value as seen in spreadsheet column 'B'
+    condition: item value as seen in spreadsheet column 'C'
+    item_name: item name as seen on the Steam marketplace
+
+    item_name format (skin): "<Weapon> | (<Condition>)"
+    item_name format (other): "<Item>"
+
+    """
+
+    for index, row in df.iterrows():
+
+        item, condition = row['Item'], row['Condition']
+        item_name = f"{item.strip()}" if pd.isnull(condition) else f"{item.strip()} ({condition.strip()})"
+
+        print(f"[{index}] {item_name}")
+
+        # Skin has been processed before so avoid calling API by retrieving info from cache
+        if item_name in items_processed and item_name in percentage_changes:
+            update_dataframe_on_success(index, item_name)
+            continue
+
+        # Skin hasn't been processed yet
+        current_value = get_current_item_value(name=item_name)
+
+        if current_value is False:  # Invalid item due to typo in file or JSON response failure, don't update item and write "n"
+            update_dataframe_failure(index)
+        else:  # Skin is valid: update processed items and percentage change cache, update item's;  value percentage change, current value and update item value modified status
+            update_cache(item_name, current_value, index)
+            update_dataframe_on_success(index, item_name)
+
+
+def update_cache(item_name, current_value, index):
+    """
+
+    Updates processed skins caches for: item current value and percentage change for item value, minimizes calls to
+    Steam endpoint
+
+    """
+
+    items_processed[item_name] = current_value
+    percentage_changes[item_name] = percentage_change(df.at[index, 'Current Value [Steam]'], current_value)
+
+
+def update_dataframe_on_success(index, item_name):
+    """
+
+    Updates 'Current Value', 'Current Value % Change', and 'Current Value Updated' columns in data frame
+    if item has been processed before or on successful response from Steam endpoint
+
+    """
+
+    df.at[index, 'Current Value % Change'] = percentage_changes[item_name]
+    df.at[index, 'Current Value [Steam]'] = items_processed[item_name]
+    df.at[index, 'Current Value Updated'] = "y"
+
+
+def update_dataframe_failure(index):
+    """Updates 'Current Value Updated' column when response from Steam endpoint is unsuccessful"""
+
+    df.at[index, 'Current Value Updated'] = "n"
+
+
+def dataframe_to_excel():
+    """Update Excel file with updated values from dataframe"""
+
+    # Write the updated "Current Value," "Current Value % Change," and "Current Value Updated" columns to the Excel file
+    for index, (current_value, current_value_change, current_value_updated) in enumerate(zip(df['Current Value [Steam]'], df['Current Value % Change'], df['Current Value Updated']), start=2):
+        cell_current_value = f'F{index}'  # Assuming the "Current Value [Steam]" column is column F
+        cell_current_value_change = f'G{index}'  # Assuming the "Current Value % Change" column is column G
+        cell_current_value_updated = f'J{index}'  # Assuming the "Current Value Updated" column is column J
+
+        ws[cell_current_value] = current_value
+        ws[cell_current_value_change] = current_value_change
+        ws[cell_current_value_updated] = current_value_updated
+
+    update_expected_percentage_change()
+    update_time_modified()
+
+
+def update_expected_percentage_change():
+    """Updates cell M1 with expected profit percentage change"""
+
+    new_expected_profit = calculate_expected_profit()
+    ws['M1'] = percentage_change(old_expected_profit, new_expected_profit)
+
+
+def update_time_modified():
+    """ Updates cell L3 with the current date and time"""
+
+    current_time = datetime.now().strftime("%d/%m/%Y at %H:%M")
+    ws['L3'] = current_time  # Writing update time into cell L3
+
+
+def save_excel():
+    """Saves the updated Excel file with the original formatting to specified directories"""
+
+    wb.save(file_path_local)
+    wb.save(file_path_desktop)
+
+
+if __name__ == "__main__":
+
+    # Load Excel spreadsheet into workbook and Pandas dataframe
+    file_path_local = '<file_name>.xlsx'
+    file_path_desktop = r'C:\Users\<your_user_name>\Desktop\<file_name>.xlsx'
+
+    df = pd.read_excel(file_path_local)
+    wb = load_workbook(file_path_local)
+    ws = wb.active
+
+    # Processing caches
+    items_processed, percentage_changes = {}, {}
+
+    # Previous expected profit to be used in % change calculation later
+    old_expected_profit = calculate_expected_profit()
+
+    update_dataframe()
+    dataframe_to_excel()
+    save_excel()
---
+++ inventory.py
@@ -0,0 +1,185 @@
+import re
+import time
+from copy import copy
+from datetime import datetime
+from selenium import webdriver
+import chromedriver_autoinstaller
+from openpyxl import load_workbook
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chromedriver_autoinstaller.install()
+
+
+def generate_driver():
+    """Generating chromedriver instance"""
+
+    chromeOption = Options()
+    chromeOption.add_argument("--headless")
+    chromeOption.add_argument("--window-size=1600,1200")
+    chromeOption.add_argument("--mute-audio")
+    chromeOption.add_argument("--log-level=3")
+    chromeOption.add_argument("--silent")
+    chromeOption.add_argument("--disable-blink-features=AutomationControlled")
+    chromeOption.add_experimental_option("excludeSwitches", ['enable-automation'])
+    driver = webdriver.Chrome(options=chromeOption)
+    return driver
+
+
+def scrape_inventory():
+    """Scrapes items from specified Steam inventory
+
+    flag: determines whether to stop scraping based on an empty inventory slot or disabled forward pagination button
+    processed_count: keeps track of how many items have been processed so seen items are not reprocessed
+    max_row: alternate max_row (starts from row 1) as opposed to ws.max_row (starts at 3 due to 'Last Price Check' cell),
+    allows population from row 1 without leaving two space gap in the spreadsheet
+
+    """
+
+    flag, processed_count, max_row = True, 0, 1
+
+    # generate chromedriver and accept cookies
+    driver = generate_driver()
+    driver.get(base_url)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'acceptAllButton'))).click()
+
+    while flag:
+
+        # find item holder elements
+        items = driver.find_elements(By.CLASS_NAME, 'itemHolder')
+
+        for i, item in enumerate(items[processed_count:]):
+
+            if 'disabled' not in item.get_attribute('class'):  # check if item slot is empty
+
+                item.click()
+                rhs = driver.find_element(By.CLASS_NAME, 'inventory_page_right')
+
+                # for some reason, the most recent item name and tag elements alternate when clicking a new item
+                if i % 2 == 1:
+                    item_name_element = rhs.find_elements(By.CLASS_NAME, 'hover_item_name')[1]
+                    item_tag_element = rhs.find_elements(By.CSS_SELECTOR, 'span.item_desc_descriptors')[1]
+                else:
+                    item_name_element = rhs.find_elements(By.CLASS_NAME, 'hover_item_name')[0]
+                    item_tag_element = rhs.find_elements(By.CSS_SELECTOR, 'span.item_desc_descriptors')[0]
+
+                item_name = item_name_element.text.strip()
+                item_tag = item_tag_element.text.strip()
+
+                print(f"[{processed_count + 1}] {item_name:^50} -> [{item_tag}]")
+                processed_count += 1
+
+                add_item_to_excel(item_name, item_tag, max_row)
+                max_row += 1
+
+            else:
+                flag = False
+                break
+
+            # if last item on the page has been processed (25 per page) find the 'next' button and check if it's disabled
+            if (i + 1) % 25 == 0:
+
+                next_button = driver.find_element(By.ID, 'pagebtn_next')
+
+                if 'disabled' in next_button.get_attribute('class'):
+                    flag = False
+                    break
+
+                next_button.click()
+                time.sleep(.5)
+
+    driver.quit()
+
+
+def add_item_to_excel(item_name, item_tag, max_row):
+    """Adds inventory item into spreadsheet as a new row with necessary information
+
+    condition: checks if item has a condition value e.g. weapon, if not then leave blank
+    item_tag: contains information relevant to the item, some examples below
+
+    [Pistol, P250, The Breakout Collection, Normal, Restricted, Factory New, Tradable, Marketable]
+    [Graffiti, Normal, Base Grade, Jungle Green, Tradable, Marketable]
+    [Music Kit, Normal, High Grade, Not Tradable, Not Marketable]
+    [Collectible, Normal, Extraordinary, Not Tradable, Not Marketable]
+    [Container, The Clutch Collection, Normal, Base Grade, Tradable, Marketable]
+
+    hence, we focus on index 0 using 'conditional_items_filter' to check if the item is a weapon before subscripting for
+    item condition
+
+    """
+
+    date = f'{datetime.now().strftime("%d/%m/%Y")}'
+    item = item_name
+    condition = f"{item_tag.split(',')[5] if item_tag.split(',')[0] in conditional_items_filter else ''}"
+    purchase_platform = "Steam"
+    purchase_price = 0.03
+    current_value = 0.03
+    percentage_change = 0.00
+    price_difference = 0.00
+    sold_price = "N/A"
+    updated = "n"
+
+    data = [date, item, condition, purchase_platform, purchase_price, current_value, percentage_change,
+            price_difference, sold_price, updated]
+
+    # create a new row
+    new_row = max_row + 1
+
+    # create new cell in new row and apply cell styling from header row
+    for col_index, cell in enumerate(row_styles, start=1):
+        new_cell = ws.cell(row=new_row, column=col_index, value=None)  # Create an empty cell
+        new_cell._style = copy(row_styles[col_index - 1]._style)  # Copy the styling
+
+    # populate cells with scraped item values
+    for j, new_cell in enumerate(data):
+        ws.cell(row=new_row, column=j + 1, value=data[j])
+
+
+def apply_difference_formula():
+    """Applies price difference formula to column H"""
+
+    # applying the formula from row 2 onward
+    for row_index in range(2, ws.max_row + 1):
+        formula_cell = f'H{row_index}'  # The cell in column H for the formula
+        formula = f'=F{row_index}-E{row_index}'  # =Fx-Ex
+        ws[formula_cell] = formula
+
+
+def save_excel():
+    """Saves the updated Excel file with the original formatting to specified directories"""
+
+    wb.save(file_path_local)
+    wb.save(file_path_desktop)
+
+
+if __name__ == "__main__":
+
+    # user Steam inventory URL input combined with #730 for CSGO
+    pattern = r'https://steamcommunity\.com/id/[\w-]+/inventory/'
+    base_url = input("Steam Inventory URL: ")
+
+    if not re.match(pattern, base_url):
+        print("Invalid URL")
+        quit(0)
+
+    base_url += "#730"
+
+    # load Excel spreadsheet into workbook
+    base_path = 'base_file.xlsx'
+    file_path_local = 'modified_spreadsheet.xlsx'
+    file_path_desktop = r'C:\Users\<your_user_name>\Desktop\modified_spreadsheet.xlsx'
+
+    wb = load_workbook(base_path)
+    ws = wb.active
+
+    # get first row of cells from column A-J so cell in column can inherit styling
+    row_styles = ws[1][:10]
+
+    # valid weapon categories to help filter for conditional items
+    conditional_items_filter = {"Rifle", "SMG", "Shotgun", "Pistol", "Sniper Rifle", "Knife", "Machinegun"}
+
+    scrape_inventory()
+    apply_difference_formula()
+    save_excel()
