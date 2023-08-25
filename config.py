14d5af8ac47a10624f747683939e1d10d8eff70b
Jonathan9168 <77795437+Jonathan9168@users.noreply.github.com>
2023-08-25 22:16:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-file_path_local = '<file_name>.xlsx' # output file in same directory as scripts
-file_path_desktop = None # file output to desktop (if you want to) e.g., r'<path_to_desktop>/<file_name>.xlsx'
-chrome_driver_executable_path = None # full path to chromedriver executable if driver not found
+file_path_local = '<file_name>.xlsx'  # output file in same directory as scripts
+file_path_desktop = None  # file output to desktop (if you want to) e.g., r'<path_to_desktop>/<file_name>.xlsx'
+chrome_driver_executable_path = None  # full path to chromedriver executable if driver not found
